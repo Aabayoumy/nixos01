012cb9958e83a7fa65402f8a4e8348747cc759ad
abayoumy@outlook.com <Ahmed Bayoumy>
2024-10-19 22:52:29 +0300

    8 current   2024-10-19 22:36:05  24.05.20241017.4eb33fe   6.6.57                          *

--- home.nix
+++ home.nix
@@ -91,8 +91,6 @@
     initExtra = ''
       PROMPT=" ◉ %U%F{magenta}%n%f%u@%U%F{blue}%m%f%u:%F{yellow}%~%f
        %F{green}→%f "
-      RPROMPT="%F{red}▂%f%F{yellow}▄%f%F{green}▆%f%F{cyan}█%f%F{blue}▆%f%F{magenta}▄%f%F{white}▂%f"
-      [ $TERM = "dumb" ] && unsetopt zle && PS1='$ '
     '';
   };
   # Let Home Manager install and manage itself.
--- update.sh
+++ update.sh
@@ -1,7 +1,28 @@
+#!/bin/sh
+## https://gist.github.com/0atman/1a5133b842f929ba4c1e195ee67599d5
+set -e
 clear
+pushd /media/data/nixos01 &>/dev/null
+if git diff --quiet '*.nix'; then
+    echo "No changes detected, exiting."
+    popd &>/dev/null
+    exit 0
+fi
 echo -e "\e[32m------ Autoformat nix files ------\e[0m"
 alejandra . &>/dev/null  || ( alejandra . ; echo "formatting failed!" && exit 1)
+# Shows your changes
+git diff -U0 '*.nix'
 echo -e "\e[32m------ Rebuild System Settings ------\e[0m"
-sudo nixos-rebuild switch --flake /media/data/nixos01#nixos01
+sudo nixos-rebuild switch --flake /media/data/nixos01#nixos01 &>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+# Get current generation metadata
+current=$(nixos-rebuild list-generations | grep current)
 echo -e "\e[32m------ Rebuild User Settings ------\e[0m"
-nix run home-manager/master -- switch --flake /media/data/nixos01#abayoumy
+nix run home-manager/master -- switch --flake /media/data/nixos01#abayoumy &>>nixos-switch.log || (cat nixos-switch.log | grep --color error && exit 1)
+echo -e "\e[32m------ Operation Completed Successfully ------\e[0m"
+
+# Commit all changes witih the generation metadata
+git add .
+git commit -am "$current"
+
+# Back to where you were
+popd &>/dev/null
