197813e27d4cf7723e70409d02f03e1fb822eab4
abayoumy@outlook.com <Ahmed Bayoumy>
2024-10-20 17:44:36 +0300

    18 current  2024-10-20 17:23:32  24.05.20241017.4eb33fe   6.6.57                          *

--- profiles/server/home.nix
+++ profiles/server/home.nix
@@ -25,6 +25,19 @@
   ];
 
   xdg.enable = true;
+  xdg.userDirs = {
+    extraConfig = {
+      XDG_DESKTOP_DIR = "${config.home.homeDirectory}/Desktop";
+      XDG_DOCUMENTS_DIR = "${config.home.homeDirectory}/Documents";
+      XDG_DOWNLOAD_DIR = "${config.home.homeDirectory}/Downloads";
+      XDG_MUSIC_DIR = "${config.home.homeDirectory}/Music";
+      XDG_PICTURES_DIR = "${config.home.homeDirectory}/Pictures";
+      XDG_PUBLICSHARE_DIR = "${config.home.homeDirectory}/Public";
+      XDG_TEMPLATES_DIR = "${config.home.homeDirectory}/Templates";
+      XDG_VIDEOS_DIR = "${config.home.homeDirectory}/Videos";
+    };
+  };
+
   xdg.userDirs.createDirectories = true;
   programs.home-manager.enable = true;
 }
