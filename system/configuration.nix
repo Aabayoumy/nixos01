46841ce6a23376c4c4bbf4a3e62cc6e1a7374224
Ahmed Bayoumy <abayoumy@outlook.com>
2024-10-23 01:08:16 +0300

    rmeove hw config - use system one

--- system/configuration.nix
+++ system/configuration.nix
@@ -11,7 +11,7 @@
 }: {
   imports = [
     # Include the results of the hardware scan.
-    /etc/nixos/hardware-configuration.nix
+    /mnt/etc/nixos/hardware-configuration.nix
     ./nfs.nix
   ];
 
